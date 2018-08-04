3b4af7eb5c19ef083877a9195d0b653546ddab82
lion <kotleta.first@gmail.com>
2018-08-04 12:43:16 +0300

    new

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,59 @@
-// function active_button(number){
-//     for(var i=0; i < menuButtons.length; i++){
-//         menuButtons[i].classList.remove('active');
-//     }
-//     menuButtons[number].classList.add('active');
-//     return this
-// }
-//
-// var menuButtons = document.querySelectorAll('.top-nav > .nav-item');
-//
-// for(var i=0; i < menuButtons.length; i++){
-//     menuButtons[i].addEventListener('click', active_button(i),false);
-// }
-//
-var currentBlock = $('.main:first-child');
-$('.top-nav > .nav-item').bind('click', function(){
-    $('.top-nav > .nav-item').removeClass('active');
-    $(this).addClass('active');
+$(document).ready(function () {
+        var currentBlock = $('.info-block .main');
+        var currentNavItem = $('.top-nav').children().eq(0)[0];
 
-    if($(this).html() === $('.top-nav').children().eq(0)[0].innerHTML){
-        $(currentBlock).addClass('fadeOutLeft');
-        $('.main').addClass('fadeInRight');
-        $('.main').addClass('active-block');
-        currentBlock = $('.main');
+        //Active navItem and current info block
+        function navBlock() {
+            $('.top-nav > .nav-item').on('click', function () {
+                $('.top-nav > .nav-item').removeClass('active');
+                $(this).addClass('active');
+                $(this).removeClass('hover');
+
+                $(currentBlock).addClass('fadeOutLeft');
+
+                setTimeout(function () {
+                    var t = $('.info-block .item');
+                    for (var k = 0;k < t.length; k++) {
+                        if ($($('.info-block .item')[k]).html() != $(currentBlock).html()) {
+                            $($('.info-block .item')[k]).removeClass('active-block');
+                        }
+                    }
+                    $('.info-block .item').removeClass('fadeOutLeft');
+                }, 1000)
+                if ($(this).html() === $('.top-nav').children().eq(0)[0].innerHTML) {
+                    activeBlock('.main', 0);
+                }
+                else if ($(this).html() === $('.top-nav').children().eq(1)[0].innerHTML) {
+                    activeBlock('.about-block', 1);
+                }
+                else if ($(this).html() === $('.top-nav').children().eq(2)[0].innerHTML) {
+                    activeBlock('.contact', 2);
+                }
+
+            });
+        }
+
+        function activeBlock(newBlock, id){
+            $(newBlock).addClass('fadeIn').one('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function () {
+                $(newBlock).removeClass('fadeIn');
+            });
+            $(newBlock).addClass('active-block');
+            currentBlock = newBlock;
+            currentNavItem = $('.top-nav').children().eq(id)[0];
+        }
+        navBlock();
+
+        //Hover on topMenuItem
+        function topMenuHover() {
+            $('.top-nav > .nav-item').hover(function () {
+                if ($(this).html() !== currentNavItem.innerHTML) {
+                    $(this).addClass('hover');
+                }
+            },
+            function(){
+                $(this).removeClass('hover');
+            })
+        }
+        topMenuHover();
     }
-    else if($(this).html() === $('.top-nav').children().eq(1)[0].innerHTML){
-        $(currentBlock).addClass('fadeOutLeft');
-        $('.about-block').addClass('fadeInRight');
-        $('.about-block').addClass('active-block');
-        currentBlock = $('.about-block');
-    }
-    else if($(this).html() === $('.top-nav').children().eq(2)[0].innerHTML){
-        $(currentBlock).addClass('fadeOutLeft');
-        $('.contact').addClass('fadeInRight')
-        $('.contact').addClass('active-block')
-        currentBlock = $('.contact');
-
-    }
-
-});
-
+)
